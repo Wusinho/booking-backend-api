ef08ca842707f9c324c386ed3dc1a64793da4fbd
Heber Lazo <heberlazo@gmail.com>
2021-09-08 16:28:06 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :coaches
-  resources :users
+  resources :appointments, only: %i[create index] 
+  resources :coaches, only: %i[create index] 
+  resources :users, only: %i[create index] 
+
+  post '/login', to: 'users#login'
+  get '/auto_login', to: 'users#auto_login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
