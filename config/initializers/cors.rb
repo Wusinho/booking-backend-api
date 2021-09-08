dcf645451e1f71d86e85e8098800790f0db38d26
Heber Lazo <heberlazo@gmail.com>
2021-09-08 18:05:55 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      headers: 'Access-Control-Allow-Origin'
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
