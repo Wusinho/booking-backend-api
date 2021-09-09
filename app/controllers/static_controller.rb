07cbed29556210bb1cef2cc897138ce052a44c8e
Heber Lazo <heberlazo@gmail.com>
2021-09-09 11:04:05 -0500

    Added root to routes

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+
+  def home
+    render json: { status: 'Welcome to Main'}
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   post '/login', to: 'users#login'
   get '/auto_login', to: 'users#auto_login'
+
+  root 'static#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
