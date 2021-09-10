a7f4ec3c4915ede38829fa3e131d5f34093b344d
Heber Lazo <heberlazo@gmail.com>
2021-09-10 12:43:08 -0500

    Corrected User status registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { 
         user: @user,
-        token: token 
+        token: token,
         status: true
       }
     else
