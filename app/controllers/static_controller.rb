0e50ddc30a82f5a6e75be74c756c666ebd8f4ef5
Heber Lazo <heberlazo@gmail.com>
2021-09-29 09:15:03 -0500

    Removed unnecessary code

--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticController < ApplicationController
-  def home
-    render json: { status: 'Welcome to Main' }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[create index show]
 
   post '/login', to: 'users#login'
-  get '/auto_login', to: 'users#auto_login'
 
-  root 'static#home'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
